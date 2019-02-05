7b85d49f1a9419cd90f75f39ccdff42d6c21a00d
Wallace <wallace@wall.org>
2019-02-05 08:22:32 -0200

    Add commit

---
+++ app.js
@@ -0,0 +1,21 @@
+(function() {
+
+  angular.module('myApp', [])
+    .controller('retanguloController', ['$scope', function($scope) {
+      
+    }])
+    .controller('trianguloController', ['$scope', function($scope) {
+      
+    }])
+    .controller('circuloController', ['$scope', function($scope) {
+      
+    }])
+    .controller('casaController', ['$scope', function($scope) {
+      
+    }])
+    .controller('figuraCompostaController', ['$scope', function($scope) {
+      
+    }]);
+    
+
+})(); 
