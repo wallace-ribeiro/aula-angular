24a9d0ceadaa091b192fe11cb9bec1b440727ad0
wallace-ribeiro <46681115+wallace-ribeiro@users.noreply.github.com>
2019-06-14 19:53:16 -0300

    Create login.service.js

---
+++ login-final/login/login.service.js
@@ -0,0 +1,26 @@
+(function() {
+
+  angular.module('myApp')
+  .service('LoginService', ['$http', function($http) {
+    
+    this.login = (login, senha) => {
+      return $http.post('http://104.248.235.252:3003/api/login',{login: login, senha: senha}).then((response) => {
+        console.log('Response: ', response);
+	      if(response.status == 200) {
+	        localStorage.setItem('token', response.data.token);
+          localStorage.setItem('username', login)
+	        return true;
+	      }
+	      return false;
+     })
+     .catch((error) => {
+       console.log('Error: ',error);
+     });
+    };
+    
+    this.logout = () => {
+      localStorage.setItem('token', '');
+    }
+  }])
+  
+})();  
